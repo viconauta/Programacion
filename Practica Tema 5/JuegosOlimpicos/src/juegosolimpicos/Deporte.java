344afa217fcecfc738b70c842261753f77df44d1
viconauta <antonio-vico@hotmail.es>
2026-02-22 21:22:55 +0100

    clase deporte creada y enum deporte, y clase juego olimpico hasta setters

---
+++ Practica Tema 5/JuegosOlimpicos/src/juegosolimpicos/Deporte.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juegosolimpicos;
+
+/**
+ *
+ * @author anton
+ */
+public class Deporte {
+    //atributos
+    private String nombre;
+    private TipoDeporte tipo;
+    private int numMaxParticipantes;
+    private Prueba[] conjuntoPruebas;
+    
+    //constructores
+    //defecto
+    public Deporte() {
+        this.nombre = "Sin nombre";
+        this.tipo = null;
+        this.numMaxParticipantes = 10;
+        this.conjuntoPruebas = new Prueba[0];
+    }
+    
+    //parametros
+    public Deporte(String n, TipoDeporte t, int num, Prueba[] c) {
+        this.nombre = n;
+        this.tipo = t;
+        this.numMaxParticipantes = num;
+        this.conjuntoPruebas = c.clone();
+    }
+    
+    //copia
+    public Deporte(Deporte d) {
+        this.nombre = d.nombre;
+        this.tipo = d.tipo;
+        this.numMaxParticipantes = d.numMaxParticipantes;
+        this.conjuntoPruebas = d.conjuntoPruebas.clone();
+    }
+    
+    //getters
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public TipoDeporte getTipo() {
+        return this.tipo;
+    }
+    
+    public int getNumMaxParticipantes() {
+        return this.numMaxParticipantes;
+    }
+    
+    public Prueba[] getConjuntoPruebas() {
+        return this.conjuntoPruebas.clone();
+    }
+    
+    //setters
+    public void setNombre(String n) {
+        this.nombre = n;
+    }
+    
+    public void setTipo(TipoDeporte t) {
+        this.tipo = t;
+    }
+    
+    public void setNumMaxParticipantes(int n) {
+        this.numMaxParticipantes = n;
+    }
+    
+    public void setConjuntoPruebas(Prueba[] c) {
+        this.conjuntoPruebas = c.clone();
+    }
+    
+    //to string
+    @Override
+    public String toString() {
+        String imprimir = "Nombre: " + this.nombre +
+                "\n Tipo: " + this.tipo +
+                "\n Numero maximo de participantes: " + this.numMaxParticipantes + 
+                "Lista de pruebas: \n";
+        for(int i = 0; i < this.conjuntoPruebas.length; i++) {
+            if(this.conjuntoPruebas[i] != null){
+                imprimir += this.conjuntoPruebas[i] + "\n";
+            }
+        }
+        return imprimir;
+    }
+}
---
+++ Practica Tema 5/JuegosOlimpicos/src/juegosolimpicos/JuegoOlimpico.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juegosolimpicos;
+
+/**
+ *
+ * @author anton
+ */
+public class JuegoOlimpico {
+    //atributos
+    private String nombreEvento, ciudadSede;
+    private int anio;
+    private Participante[] listaParticipantes;
+    private Deporte[] listaDeportes;
+    
+    //constructores
+    //defecto
+    public JuegoOlimpico() {
+        this.nombreEvento = "Sin nombre";
+        this.anio = 2026;
+        this.ciudadSede = "Sin ciudad";
+        this.listaParticipantes = new Participante[0];
+        this.listaDeportes = new Deporte[0];
+    }
+    
+    //parametros
+    public JuegoOlimpico(String n, int anio, String ciudad, Participante[] p, Deporte[] d) {
+        this.nombreEvento = n;
+        this.anio = anio;
+        this.ciudadSede = ciudad;
+        this.listaParticipantes = p.clone();
+        this.listaDeportes = d.clone();
+    }
+    
+    //copia
+    public JuegoOlimpico(JuegoOlimpico jo) {
+        this.nombreEvento = jo.nombreEvento;
+        this.anio = jo.anio;
+        this.ciudadSede = jo.ciudadSede;
+        this.listaParticipantes = jo.listaParticipantes.clone();
+        this.listaDeportes = jo.listaDeportes.clone();
+    }
+    
+    //getters
+    public String getNombreEvento() {
+        return this.nombreEvento;
+    }
+    
+    public int getAnio() {
+        return this.anio;
+    }
+    
+    public String getCiudadSede() {
+        return this.ciudadSede;
+    }
+    
+    public Participante[] getListaParticipantes() {
+        return this.listaParticipantes.clone();
+    }
+    
+    public Deporte[] getListaDeportes() {
+        return this.listaDeportes.clone();
+    }
+    
+    //setters
+    public void setNombreEvento(String n) {
+        this.nombreEvento = n;
+    }
+    
+    public void setAnio(int a) {
+        this.anio = a;
+    }
+    
+    public void setCiudadSede(String c) {
+        this.ciudadSede = c;
+    }
+    
+    public void setListaParticipantes(Participante[] p) {
+        this.listaParticipantes = p.clone();
+    }
+    
+    public void setListaDeportes(Deporte[] d) {
+        this.listaDeportes = d.clone();
+    }
+    
+    //metodos
+}
